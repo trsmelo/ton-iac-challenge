1efcf5c2be4ce73cfdcbd22abb2f676f044fc3d5
trsmelo <tiago.melo@uniriotec.br>
2021-05-28 00:10:25 -0300

    Add alarm resource

--- infra/main.tf
+++ infra/main.tf
@@ -87,6 +87,24 @@ resource "aws_eip" "ubuntu" {
 
 }
 
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+  alarm_name = "cpu-utilization-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "80"
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+
+  dimensions = {
+    InstanceId = aws_instance.ubuntu.id
+  }
+  
+}
+
 output "Public_DNS" {
   value = aws_eip.ubuntu.public_dns
 }
